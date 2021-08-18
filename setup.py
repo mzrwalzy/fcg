f1e2b5ecdfe39e9d572de4a49b1168b57eef720e
MENG <mzrwalzy@163.com>
2021-08-18 13:30:09 +0800

    complete

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='fcg',
+    version='0.1.0',
+    author='charon.',
+    author_email='mzrwalzy@163.com',
+    description='fastapi code generator',
+    long_description_content_type='text/markdown',
+    url='https://github.com/mzrwalzy/fcg.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
